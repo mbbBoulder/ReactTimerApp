4ffbb958588c7e8529ca13abf6b9953413132f70
Scott McLamb <scott.mclamb@gmail.com>
2017-01-15 13:04:53 -0700

    Finalize tests and page title.

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -11,13 +11,13 @@ describe('Clock', () => {
     expect(Countdown).toExist();
   });
 
-  describe('handleSetCountdown', (done) => {
-    it('should set state to started and count down', () => {
+  describe('handleSetCountdown', () => {
+    it('should set state to started and count down', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleSetCountdown(10);
 
       expect(countdown.state.count).toBe(10);
-      expect(countdown.state.countDownStatus).toBe('started');
+      expect(countdown.state.countdownStatus).toBe('started');
 
       setTimeout(() => {
         expect(countdown.state.count).toBe(9);
@@ -25,7 +25,7 @@ describe('Clock', () => {
       }, 1001);
     });
 
-    it('should never let count be negative', () => {
+    it('should never let count be negative', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleSetCountdown(1);
 
@@ -33,6 +33,28 @@ describe('Clock', () => {
         expect(countdown.state.count).toBe(0);
         done();
       }, 3001);
+    });
+
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should reset count on stopped status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
