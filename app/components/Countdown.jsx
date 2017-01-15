5b566260882503f19bfbae607619143a4c270956
Scott McLamb <scott.mclamb@gmail.com>
2017-01-14 21:10:19 -0700

    Add clock component / style

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function () {
-    return <p>Countdown</p>;
+    return (
+      <Clock totalSeconds={129}/>
+    );
   }
 });
 
